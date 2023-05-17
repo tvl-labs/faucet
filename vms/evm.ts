b06d1a72d8ceb37fe63356ef188b3eb94b8f0bb7
serejke <serejke.best@gmail.com>
2023-05-17 21:55:20 +0100

    refactor: pull from the status map.

--- vms/evm.ts
+++ vms/evm.ts
@@ -10,12 +10,10 @@ import { Account } from 'web3-core';
 import { ChainType, ERC20Type } from '../types';
 
 // cannot issue tx if no. of pending requests is > 16
-const MEMPOOL_LIMIT = 15
+const MEM_POOL_LIMIT = 15
 
-// pending tx timeout should be a function of MEMPOOL_LIMIT
+// pending tx timeout should be a function of MEM_POOL_LIMIT
 const PENDING_TX_TIMEOUT = 40 * 1000 // 40 seconds
-
-const BLOCK_FAUCET_DRIPS_TIMEOUT = 60 * 1000 // 60 seconds
 
 export default class EVM {
     web3: Web3
@@ -36,13 +34,9 @@ export default class EVM {
     nonce: number
     balance: BN
     isRecalibrating: boolean
-    waitArr: RequestType[]
-    queue: (RequestType & { nonce: number})[]
     error: boolean
     log: Log
 
-    queuingInProgress: boolean
-    isDelayedStart: boolean
     lastRecalibrationTimestamp: number;
     private config: ChainType;
 
@@ -63,14 +57,9 @@ export default class EVM {
         this.balance = new BN(0)
 
         this.isRecalibrating = false
-        this.queuingInProgress = false
         this.lastRecalibrationTimestamp = 0
 
-        this.waitArr = []
-        this.queue = []
-
         this.error = false
-        this.isDelayedStart = true
     }
 
     async start() {
@@ -78,11 +67,7 @@ export default class EVM {
 
         setInterval(() => this.recalibrate(), 1000);
 
-        // block requests during restart (to settle any pending txs initiated during shutdown)
-        setTimeout(() => {
-            this.log.info("Faucet is ready to start serving requests...")
-            this.isDelayedStart = false
-        }, BLOCK_FAUCET_DRIPS_TIMEOUT);
+        setInterval(() => this.schedule(), 1000);
     }
 
     // Setup Legacy or EIP1559 transaction type
@@ -94,38 +79,29 @@ export default class EVM {
     // Function to issue transfer transaction. For ERC20 transfers, 'id' will be a string representing ERC20 token ID
     async sendToken(
         receiver: string,
-        id: string | undefined
+        erc20: string | undefined
     ): Promise<SendTokenResponse> {
-        if (this.isDelayedStart) {
-            return { status: 400, message: "Faucet is getting started! Please try after sometime"};
-        }
-
         if (!this.web3.utils.isAddress(receiver)) {
-            return { status: 400, message: "Invalid address! Please try again." };
+            return { status: 400, message: `Invalid address ${receiver}` };
         }
 
-        // do not accept any request if mempool limit reached
-        if (this.requestStatus.size >= MEMPOOL_LIMIT) {
-            this.log.error(`Reached the mempool limit of ${MEMPOOL_LIMIT}`);
+        if (this.requestStatus.size >= MEM_POOL_LIMIT) {
+            this.log.error(`Reached the mem pool limit of ${MEM_POOL_LIMIT}`);
             return { status: 400, message: "High faucet usage! Please try after sometime" };
         }
 
         let amount: BN = calculateBaseUnit(this.config.DRIP_AMOUNT.toString(), this.config.DECIMALS || 18)
 
         // If id is provided, then it is ERC20 token transfer, so update the amount
-        if (id) {
-            const erc20 = this.contracts.get(id)!;
-            amount = calculateBaseUnit(erc20.dripAmount.toString(), erc20.decimals || 18)
+        if (erc20) {
+            const contract = this.contracts.get(erc20)!;
+            amount = calculateBaseUnit(contract.dripAmount.toString(), contract.decimals || 18)
         }
 
-        const requestId = receiver + id + Math.random().toString()
+        const requestId = receiver + erc20 + Math.random().toString()
 
-        const request: RequestType = { receiver, amount, id, requestId };
-        if (this.isRecalibrating) {
-            this.waitArr.push(request)
-        } else {
-            this.putInQueue(request)
-        }
+        const request: RequestType = { receiver, amount, id: erc20, requestId };
+        this.requestStatus.set(requestId, { type: 'mem-pool', request });
 
         return new Promise((resolve) => {
             const statusCheckerInterval = setInterval(async () => {
@@ -135,7 +111,7 @@ export default class EVM {
                     this.requestStatus.delete(requestId);
                     switch (requestStatus.type) {
                         case 'pending':
-                            resolve({ status: 200, message: `Transactio sent on ${this.config.NAME}`, txHash: requestStatus.txHash })
+                            resolve({ status: 200, message: `Transaction sent on ${this.config.NAME}`, txHash: requestStatus.txHash })
                             break;
                         case 'error':
                             const errorMessage = requestStatus?.errorMessage
@@ -174,73 +150,51 @@ export default class EVM {
         }
     }
 
-    balanceCheck(req: RequestType): Boolean {
-        if (req.id) {
-            const contract = this.contracts.get(req.id)!;
-            if (contract.balance.gte(req.amount)) {
-                contract.balance = contract.balance.sub(req.amount)
-                return true
-            }
+    isFaucetBalanceEnough(request: RequestType): boolean {
+        if (request.id) {
+            const contract = this.contracts.get(request.id)!;
+            return contract.balance.gte(request.amount);
         } else {
-            if (this.balance.gte(req.amount)) {
-                this.balance = this.balance.sub(req.amount)
-                return true
-            }
+            return this.balance.gte(request.amount);
         }
-        return false
     }
 
-    putInQueue(req: RequestType) {
-        this.queuingInProgress = true
+    async processRequest(request: RequestType) {
+        if (this.isRecalibrating) {
+            this.requestStatus.set(request.requestId, { type: 'mem-pool', request })
+            return;
+        }
 
-        if (!this.balanceCheck(req)) {
-            this.queuingInProgress = false
-            this.log.warn("Faucet balance is too low! " + req.id + " " + this.getBalance(req.id))
-            this.requestStatus.set(req.requestId, {
+        const { amount, receiver, id } = request;
+
+        if (!this.isFaucetBalanceEnough(request)) {
+            this.log.error(`Faucet balance is too low! ${request.id}: ${this.getBalance(request.id)}`)
+            this.requestStatus.set(request.requestId, {
                 type: "error",
                 errorMessage: "Faucet balance is too low! Please try later."
             })
-            return
+            return;
         }
 
-        this.queue.push({ ...req, nonce: this.nonce })
-        this.nonce++
-
-        const request = this.queue.shift()!;
-        this.sendTokenUtil(request);
-    }
-
-    async sendTokenUtil(request: RequestType & { nonce: number}) {
-        const { amount, receiver, nonce, id } = request;
-
-        // request from queue is now moved to pending txs list
-        this.queuingInProgress = false
+        const nonce = this.nonce;
+        this.nonce++;
 
         const { txHash, rawTransaction } = await this.getTransaction(receiver, amount, nonce, id)
         this.requestStatus.set(request.requestId, { type: "pending", txHash });
 
-        /*
-        * [CRITICAL]
-        * If a issued tx fails/timed-out, all succeeding nonce will stuck
-        * and we need to cancel/re-issue the tx with higher fee.
-        */
         try {
-            /*
-            * asyncCallWithTimeout function can return
-            * 1. successfull response
-            * 2. throw API error (will be catched by catch block)
-            * 3. throw timeout error (will be catched by catch block)
-            */
             await asyncCallWithTimeout(
-                this.web3.eth.sendSignedTransaction(rawTransaction),
-                PENDING_TX_TIMEOUT,
-                `Timeout reached for transaction with nonce ${nonce}`,
+              this.web3.eth.sendSignedTransaction(rawTransaction),
+              PENDING_TX_TIMEOUT,
+              `Timeout reached for transaction ${txHash} with nonce ${nonce}`,
             )
-
-            this.requestStatus.set(request.requestId, { type: 'confirmed', txHash})
         } catch (err: any) {
-            this.log.error(err.message)
+            this.log.error(err.message);
+            this.requestStatus.set(request.requestId, { type: 'error', errorMessage: err.message })
+            return;
         }
+
+        this.requestStatus.set(request.requestId, { type: 'confirmed', txHash})
     }
 
     async getTransaction(
@@ -288,6 +242,20 @@ export default class EVM {
         return Math.min(adjustedGas, parseInt(this.config.MAX_FEE))
     }
 
+    schedule() {
+        const entry = Array.from(this.requestStatus.entries())
+          .find(([, request]) => request.type === 'mem-pool');
+        if (!entry) {
+            return;
+        }
+        const [requestId, transactionStatus] = entry;
+        this.requestStatus.delete(requestId);
+        if (transactionStatus.type === "mem-pool") {
+            const request = transactionStatus.request;
+            this.processRequest(request).catch((e: any) => this.log.error(e.message));
+        }
+    }
+
     async recalibrate(): Promise<void> {
         if (this.isRecalibrating) {
             return;
@@ -296,20 +264,13 @@ export default class EVM {
         const nowTimestamp = Date.now();
         const isTimeToRecalibrate = (nowTimestamp - this.lastRecalibrationTimestamp) / 1000 > (this.config.RECALIBRATE || 30);
 
-        const isIdle = this.requestStatus.size === 0 && !this.queuingInProgress;
-        if (isIdle && isTimeToRecalibrate) {
+        if (this.requestStatus.size === 0 && isTimeToRecalibrate) {
             this.lastRecalibrationTimestamp = nowTimestamp;
             this.isRecalibrating = true
             try {
                 await this.updateNonceAndBalance()
             } finally {
                 this.isRecalibrating = false
-            }
-        }
-
-        if (isIdle) {
-            while (this.waitArr.length !== 0) {
-                this.putInQueue(this.waitArr.shift()!);
             }
         }
     }
@@ -330,6 +291,6 @@ export default class EVM {
     }
 
     getFaucetUsagePercentage(): number {
-        return 100 * (this.requestStatus.size / MEMPOOL_LIMIT)
+        return 100 * (this.requestStatus.size / MEM_POOL_LIMIT)
     }
 }
